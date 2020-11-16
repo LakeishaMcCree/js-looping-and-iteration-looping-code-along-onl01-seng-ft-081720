49efea5e726b7f33a6b9690dfd2109b6c615a755
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-11-16 12:15:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cards = []
+function writeCards(array, string) {
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    while (number >= 0){
+        console.log(number--)
+    }
+}
+
